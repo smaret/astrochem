344060e05e94ca39c5ba422718ff5fc5d71efdf0
Mathieu Westphal <mathieu.westphal@gmail.com>
2013-08-28 14:37:43 +0200

    Adding watchdog on solve call for dynamic source

--- src/astrochem.c
+++ src/astrochem.c
@@ -1,7 +1,7 @@
 /* 
    Astrochem - compute the abundances of chemical species in the
    interstellar medium as as function of time.
-   
+
    Copyright (c) 2006-2013 Sebastien Maret
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -37,7 +37,7 @@
 void usage (void);
 void version (void);
 
-int
+  int
 main (int argc, char *argv[])
 {
   inp_t input_params;
@@ -45,7 +45,7 @@ main (int argc, char *argv[])
   net_t network;
   res_t results;
   int cell_index;
-  
+
   int verbose = 1;
   char *input_file;
 
@@ -54,7 +54,7 @@ main (int argc, char *argv[])
 
   {
     int opt;
-    
+
     static struct option longopts[] = {
       {"help",    no_argument, NULL, 'h'},
       {"version", no_argument, NULL, 'V'},
@@ -62,49 +62,49 @@ main (int argc, char *argv[])
       {"quiet",   no_argument, NULL, 'q'},
       {0, 0, 0, 0}
     };
-    
+
     while ((opt = getopt_long(argc, argv, "hVvq", longopts, NULL)) != -1)
+    {
+      switch (opt)
       {
-	switch (opt)
-	  {
-	  case 'h':
-	    usage ();
-	    exit (0);
-	    break;
-	  case 'V':
-	    version ();
-	    exit (0);
-	    break;
-	  case 'v':
-	    verbose = 2;
-	    break;
-	  case 'q':
-	    verbose = 0;
-	    break;
-	  default:
-	    usage ();
-	    exit (1);
-	  }
-      };
+        case 'h':
+          usage ();
+          exit (0);
+          break;
+        case 'V':
+          version ();
+          exit (0);
+          break;
+        case 'v':
+          verbose = 2;
+          break;
+        case 'q':
+          verbose = 0;
+          break;
+        default:
+          usage ();
+          exit (1);
+      }
+    };
     argc -= optind;
     argv += optind;
     if (argc != 1) 
-      {
-	usage ();
-	exit (1);
-      }
+    {
+      usage ();
+      exit (1);
+    }
     input_file = argv[0];
   }
 
- /* Read the input file */
+  /* Read the input file */
   read_input_file_names (input_file, &input_params.files, verbose);
-  
+
   /* Read the chemical network file */
   read_network (input_params.files.chem_file, &network, verbose);
-   
+
   /* Read the input file */
   read_input (input_file, &input_params, &network , verbose);
-  
+
   /* Read the source model file */
   read_source (input_params.files.source_file, &source_mdl, verbose);
 
@@ -112,35 +112,35 @@ main (int argc, char *argv[])
      not contain any specie that is not in the network. */
 
   /*check_species (input_params.abundances.initial_abundances, input_params.abundances.n_initial_abundances,
-		 input_params.output.output_species, input_params.output.n_output_species, network.species, 
-		 network.n_species);*/
+    input_params.output.output_species, input_params.output.n_output_species, network.species, 
+    network.n_species);*/
 
   /* Allocate results */
-   alloc_results( &results, input_params.output.time_steps, source_mdl.n_cells, input_params.output.n_output_species);
+  alloc_results( &results, input_params.output.time_steps, source_mdl.n_cells, input_params.output.n_output_species);
 
 
   /* Build the vector of time */
 
   {
     int i;
-    
+
     for (i = 0; i <  input_params.output.time_steps; i++)
+    {
+      if (i < MAX_TIME_STEPS)
+        results.tim[i] = pow (10., log10 ( input_params.solver.ti) + i 
+            * (log10 (input_params.solver.tf) - log10 (input_params.solver.ti)) 
+            / (input_params.output.time_steps - 1));
+      else
       {
-	if (i < MAX_TIME_STEPS)
-	    results.tim[i] = pow (10., log10 ( input_params.solver.ti) + i 
-				   * (log10 (input_params.solver.tf) - log10 (input_params.solver.ti)) 
-				   / (input_params.output.time_steps - 1));
-	else
-	  {
-	    fprintf (stderr, "astrochem: error: the number of time" 
-		     "steps in %s exceed %i.\n", input_file, MAX_TIME_STEPS);
+        fprintf (stderr, "astrochem: error: the number of time" 
+            "steps in %s exceed %i.\n", input_file, MAX_TIME_STEPS);
         free_input (&input_params);
         free_mdl (&source_mdl);
         free_network (&network);
         free_results (&results);
-	    return(EXIT_FAILURE);
-	  }
+        return(EXIT_FAILURE);
       }
+    }
   }
 
   /* Solve the ODE system for each cell. */
@@ -154,19 +154,25 @@ main (int argc, char *argv[])
 #ifdef HAVE_OPENMP
 #pragma omp for schedule (dynamic, 1) nowait
 #endif
-
-    for (cell_index = 0; cell_index < source_mdl.n_cells; cell_index++)
+    if( source_mdl.mode==STATIC)
+    {
+      for (cell_index = 0; cell_index < source_mdl.n_cells; cell_index++)
       {
-	if (verbose >= 1)
-	  fprintf (stdout, "Computing abundances in cell %d...\n", cell_index);
-	solve (cell_index, &input_params, &source_mdl.cell[cell_index], &network, &results, verbose);
-	if (verbose >= 1)
-	  fprintf (stdout, "Done with cell %d.\n", cell_index);
+        if (verbose >= 1)
+          fprintf (stdout, "Computing abundances in cell %d...\n", cell_index);
+        solve (cell_index, &input_params, &source_mdl.cell[cell_index], &network, &results, verbose);
+        if (verbose >= 1)
+          fprintf (stdout, "Done with cell %d.\n", cell_index);
       }
+    }
+    else
+    {
+      printf("Dynamic solve to be implemented");
+    }
+
+
   }
-
   /* Write the abundances in output files */
-
   output (source_mdl.n_cells, &input_params, &network, &results, verbose);
   free_input (&input_params);
   free_mdl (&source_mdl);
@@ -176,10 +182,10 @@ main (int argc, char *argv[])
 }
 
 /*
-  Display help message.
-*/
+   Display help message.
+ */
 
-void
+  void
 usage (void)
 {
   fprintf (stdout, "Usage: astrochem [option...] [file]\n\n");
@@ -194,10 +200,10 @@ usage (void)
 }
 
 /*
-  Display version.
-*/
+   Display version.
+ */
 
-void
+  void
 version (void)
 {
   fprintf (stdout, "This is astrochem, version %s\n", PACKAGE_VERSION);
