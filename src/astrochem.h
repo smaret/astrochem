d02a651529df077543847f80e76c8f3f0147c790
Mathieu Westphal <mathieu.westphal@gmail.com>
2013-09-25 12:25:03 +0200

    typo

--- src/astrochem.h
+++ src/astrochem.h
@@ -182,6 +182,7 @@ typedef struct
    Fontions names with a _new suffix correspond to the new prototypes
    that uses the new "input_params", "source_mdl", "network" and
    "results" data structures. */
+
 void alloc_input (inp_t * input_params, int n_initial_abundances,
 		  int n_output_abundances);
 
