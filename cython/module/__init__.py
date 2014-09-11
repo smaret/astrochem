cb261d21e4195f9b30003d1b1bcb7792a17580be
Mathieu Westphal <mathieu.westphal@gmail.com>
2014-09-11 12:54:02 +0200

    Fix cython bug
    The __init__.py file was missing.

---
+++ cython/module/__init__.py
@@ -0,0 +1,7 @@
+from libpyastrochem import Solver
+from libpyastrochem import Cell
+from libpyastrochem import Network
+from libpyastrochem import Phys
+from libpyastrochem import _ABS_ERR_DEFAULT
+from libpyastrochem import _REL_ERR_DEFAULT
+
