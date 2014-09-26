9790b1ae77b31308985f60e635f498fb4ef2bbd1
Mathieu Westphal <mathieu.westphal@gmail.com>
2014-09-26 09:09:48 +0200

    Single __init__.py file for the Python module

--- python/module/__init__.py
+++ python/module/__init__.py
@@ -1,7 +1,10 @@
-from libpyastrochem import Solver
-from libpyastrochem import Cell
-from libpyastrochem import Network
-from libpyastrochem import Phys
-from libpyastrochem import _ABS_ERR_DEFAULT
-from libpyastrochem import _REL_ERR_DEFAULT
+try:
+    from libpyastrochem import Solver
+    from libpyastrochem import Cell
+    from libpyastrochem import Network
+    from libpyastrochem import Phys
+    from libpyastrochem import _ABS_ERR_DEFAULT
+    from libpyastrochem import _REL_ERR_DEFAULT
+except ImportError:
+    print "python lib api not available, only tools have been loaded."
 import libastrochemtools as tools
--- python/moduleNoCython/__init__.py
+++
@@ -1,1 +0,0 @@
-import libastrochemtools as tools
