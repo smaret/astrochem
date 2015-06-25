smaret/astrochem

commit dc646f5b6bee8250d5d21c6c6bf3c44d7dde70e3

New output format
Astrochem creates a single output file in HDF5 format. The file contains
the abundances and the formation/destruction routes of all the species that
are listed in the input file. Closes #17.

src/Makefile.am
@@ -1,7 +1,7 @@
 #  Makefile.am - Automake file for Astrochem
 #
 #  Copyright (c) 2006-2014 Sebastien Maret
-# 
+#
 #  This file is part of Astrochem.
 #
 #  Astrochem is free software: you can redistribute it and/or modify
@@ -23,14 +23,17 @@ AM_CFLAGS += ${OPENMP_CFLAGS} -Wall
 
 
 lib_LTLIBRARIES = libastrochem.la
-libastrochem_la_SOURCES = input.c network.c rates.c solve.c output.c
+libastrochem_la_SOURCES = network.c rates.c solve.c
 libastrochem_la_LDFLAGS = -version-info 0:0:0
 include_HEADERS = libastrochem.h
 
 bin_PROGRAMS = astrochem apiuser chmconvert
 
-astrochem_SOURCES = astrochem.c
-astrochem_LDADD = -lastrochem
+astrochem_SOURCES = astrochem.c input.c
+astrochem_CFLAGS = ${HDF5_CFLAGS}
+astrochem_CPPFLAGS = ${HDF5_CPPFLAGS}
+astrochem_LDFLAGS = ${HDF5_LDFLAGS}
+astrochem_LDADD = -lastrochem ${HDF5_LIBS}
 
 apiuser_SOURCES = apiuser.c
 apiuser_LDADD = -lastrochem

tests/Makefile.am
@@ -19,18 +19,18 @@
 
 AM_CFLAGS = -Wall
 
-TESTS = input_test source_test source_dyn_test network_test rate_test full_solve_test abundances_test solver_test python_lib_test python_tools_test
+TESTS = input_test source_test source_dyn_test network_test rate_test abundances_test solver_test python_lib_test python_tools_test
 
-check_PROGRAMS = input_test source_test source_dyn_test network_test rate_test full_solve_test abundances_test solver_test python_lib_test python_tools_test
+check_PROGRAMS = input_test source_test source_dyn_test network_test rate_test abundances_test solver_test python_lib_test python_tools_test
 
 input_test_SOURCES = input_test.c
-input_test_LDADD = $(top_builddir)/src/libastrochem.la
+input_test_LDADD = $(top_builddir)/src/input.o $(top_builddir)/src/libastrochem.la
 
 source_test_SOURCES = source_test.c
-source_test_LDADD = $(top_builddir)/src/libastrochem.la
+source_test_LDADD = $(top_builddir)/src/libastrochem.la  $(top_builddir)/src/input.o
 
 source_dyn_test_SOURCES = source_dyn_test.c
-source_dyn_test_LDADD = $(top_builddir)/src/libastrochem.la
+source_dyn_test_LDADD = $(top_builddir)/src/libastrochem.la $(top_builddir)/src/input.o
 
 network_test_SOURCES = network_test.c
 network_test_LDADD = $(top_builddir)/src/libastrochem.la
@@ -38,14 +38,15 @@ network_test_LDADD = $(top_builddir)/src/libastrochem.la
 rate_test_SOURCES = rate_test.c
 rate_test_LDADD = $(top_builddir)/src/libastrochem.la
 
-full_solve_test_SOURCES = full_solve_test.c
-full_solve_test_LDADD = $(top_builddir)/src/libastrochem.la
-
 abundances_test_SOURCES = abundances_test.c
 abundances_test_LDADD = $(top_builddir)/src/libastrochem.la
 
 solver_test_SOURCES = solver_test.c
 solver_test_LDADD = $(top_builddir)/src/libastrochem.la
+
+astrochem_test$(EXEEXT): astrochem_test.py
+	cp $? $@
+	chmod a+x $@
 
 python_lib_test$(EXEEXT): python_lib_test.py
 	cp $? $@

configure.ac
@@ -145,6 +145,12 @@ if test $HAVE_PYMOD_BREATHE = "no"; then
 fi
 AM_CONDITIONAL([HAVE_PYMOD_BREATHE], [test $HAVE_PYMOD_BREATHE = "yes"])
 
+#check for hdf5
+AX_LIB_HDF5()
+if test "$with_hdf5" = "no"; then
+    AC_MSG_ERROR([Unable to find HDF5, cannot compile astrochem.])
+fi
+
 AC_CONFIG_FILES([Makefile
                  doc/Makefile
 		 doc/astrochem.tex
@@ -167,6 +173,7 @@ AC_CONFIG_FILES([Makefile
 		 python/libastrochemtools/libastrochemtools.py
 		 python/libastrochemtools/Makefile
 		 python/libpyastrochem/Makefile
+         tests/astrochem_test.py
          tests/python_lib_test.py
          tests/python_tools_test.py
          tests/benchmark.py

src/astrochem.h
@@ -1,0 +1,112 @@
+/*
+   astrochem.h - Function prototypes, various constant and data
+   structures for Astrochem.
+
+   Copyright (c) 2006-2013 Sebastien Maret
+
+   This file is part of Astrochem.
+
+   Astrochem is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   Astrochem is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Astrochem.  If not, see <http://www.gnu.org/licenses/>.
+   */
+
+/* Various definitions and constants */
+#ifndef _ASTROCHEM_H_
+#define _ASTROCHEM_H_
+
+#include <hdf5.h>
+#include "libastrochem.h"
+
+#define MAX_CHAR_FILENAME 64
+
+/**
+ * @brief struct containing a rates of a reaction
+ */
+typedef struct
+{
+  int reaction_no; /*!< number of concerned reaction */
+  double rate;     /*!< rate value */
+} r_t;
+
+/**
+ * @brief struct containing a route
+ */
+typedef struct
+{
+  r_t destruction; /*!< rate of destruction */
+  r_t formation;   /*!< rate of formation */
+} rout_t;
+
+
+/**
+ * @brief struct containing output parameters
+ */
+typedef struct
+{
+  int *output_species_idx; /*!< array of output species idx */
+  int n_output_species;    /*!< number of output species */
+  int time_steps;          /*!< time steps used */
+  int trace_routes;        /*!< If routes have been traced */
+  char suffix[MAX_LINE];   /*!< Suffix to sue in output files */
+} output_t;
+
+/**
+ * @brief struct containing file names of chem file and source file
+ */
+typedef struct
+{
+  char chem_file[MAX_LINE]; /*!< Path to chem file */
+  char source_file[MAX_LINE]; /*!< Path to source file */
+} files_t;
+
+/**
+ * @brief struct containing input parametrs
+ */
+typedef struct
+{
+  files_t files;    /*!< Input files */
+  phys_t phys;      /*!< Physics parameters */
+  solver_t solver;  /*!< Solver parameters */
+  abundances_t abundances; /*!< abundances */
+  output_t output; /*!< Output parameters */
+} inp_t;
+
+
+/**
+ * @brief struct containing cell parameters
+ */
+typedef struct
+{
+  double *av;    /*!< av */
+  double *nh;    /*!< density */
+  double *tgas;  /*!< gas temperature */
+  double *tdust; /*!< dust temperature */
+} cell_table_t;
+
+/**
+ * @brief struct containing a source model
+ */
+typedef struct
+{
+  cell_table_t *cell;    /*!< Array of cells */
+  time_steps_t ts; /*!< Time steps */
+  int n_cells;     /*!< Number of cells */
+  SOURCE_MODE mode; /*!< Source mode */
+} mdl_t;
+
+
+int full_solve (hid_t fid, hid_t dataset, hid_t* routeDatasets, hid_t dataspace, hid_t routeDataspace, hid_t datatype, hid_t routeDatatype,
+                int cell_index, const inp_t * input_params, SOURCE_MODE mode,
+                const cell_table_t * cell, const net_t * network, const time_steps_t * ts, int verbose);
+
+#endif // _ASTROCHEM_H_

src/input.h
@@ -25,11 +25,26 @@
 #ifndef _INPUT_H_
 #define _INPUT_H_
 
+#include "astrochem.h"
+
+void read_input (const char *input_file, inp_t * input_params,
+                 const net_t * network, int verbose);
+
+void read_input_file_names (const char *input_file, files_t * files,
+                            int verbose);
+
+void free_input (inp_t * input_params);
+
+
 void alloc_input (inp_t * input_params, int n_initial_abundances,
                   int n_output_abundances);
 
-int get_nb_active_line (const char *file);
 
 void input_error (const char *input_f, int line_number);
+
+void read_source (const char *source_file, mdl_t * source_mdl,
+                  const inp_t * input_params, const int verbose);
+
+void free_mdl (mdl_t * source_mdl);
 
 #endif // _INPUT_H_

src/libastrochem.h
@@ -26,6 +26,7 @@
 #define _LIBASTROCHEM_H_
 
 #include <nvector/nvector_serial.h>
+#include <string.h>
 
 #define MAX_LINE 512            /* Maximum number of characters in each input file
                                    line */
@@ -70,12 +71,6 @@ typedef struct
   double abundance;
 } abund_t;
 
-typedef struct
-{
-  char chem_file[MAX_LINE];
-  char source_file[MAX_LINE];
-} files_t;
-
 /**
  * @brief struct containing physics parameters
  */
@@ -102,32 +97,6 @@ typedef struct
   int n_initial_abundances;
 } abundances_t;
 
-typedef struct
-{
-  int *output_species_idx;
-  int n_output_species;
-  int time_steps;
-  int trace_routes;
-  char suffix[MAX_LINE];
-} output_t;
-
-typedef struct
-{
-  files_t files;
-  phys_t phys;
-  solver_t solver;
-  abundances_t abundances;
-  output_t output;
-} inp_t;
-
-typedef struct
-{
-  double *av;    /*!< av */
-  double *nh;    /*!< density */
-  double *tgas;  /*!< gas temperature */
-  double *tdust; /*!< dust temperature */
-} cell_table_t;
-
 /**
  * @brief struct containing cell parameters
  */
@@ -149,13 +118,6 @@ typedef struct
   int n_time_steps;
 } time_steps_t;
 
-typedef struct
-{
-  cell_table_t *cell;    /*!< Array of cells */
-  time_steps_t ts; /*!< Time steps */
-  int n_cells;     /*!< Number of cells */
-  SOURCE_MODE mode; /*!< Source mode */
-} mdl_t;
 
 
 typedef struct
@@ -183,27 +145,9 @@ typedef struct
   react_t *reactions;
 } net_t;
 
-typedef struct
-{
-  int reaction_no;
-  double rate;
-} r_t;
-
-typedef struct
-{
-  r_t destruction;
-  r_t formation;
-} rout_t;
-
-typedef struct
-{
-  double *abundances;
-  rout_t *routes;
-  int n_cells;
-  int n_time_steps;
-  int n_output_abundances;
-} res_t;
-
+/**
+ * @brief bool enum
+ */
 typedef enum { false, true } bool;
 
 typedef struct
@@ -250,40 +194,11 @@ int solve( astrochem_mem_t* astrochem_mem, const net_t* network,
 void solver_close( astrochem_mem_t* astrochem_mem );
 
 
-void read_input (const char *input_file, inp_t * input_params,
-                 const net_t * network, int verbose);
-
-void read_input_file_names (const char *input_file, files_t * files,
-                            int verbose);
-
-void free_input (inp_t * input_params);
-
-void read_source (const char *source_file, mdl_t * source_mdl,
-                  const inp_t * input_params, const int verbose);
-
-void free_mdl (mdl_t * source_mdl);
-
-/*void check_species (abund_t initial_abundances[], int
-                    n_initial_abundances, char *output_species[], int
-                    n_output_species, char *species[], int n_species);*/
 
 void read_network (const char *chem_file, net_t * network, const int verbose);
 
 void free_network (net_t * network);
 
-void alloc_results (res_t * results, int n_time_steps, int n_cells,
-                    int n_output_abundances);
-
-void free_results (res_t * results);
-
-int full_solve (int cell_index, const inp_t * input_params, SOURCE_MODE mode,
-                const cell_table_t * cell, const net_t * network,
-                const time_steps_t * ts, res_t * results, int verbose);
-
-
-void output (int n_cells, const inp_t * input_params,
-             const mdl_t * source_mdl, const net_t * network,
-             const res_t * results, int verbose);
-
+int get_nb_active_line (const char *file);
 
 #endif // _LIBASTROCHEM_H_

src/network.c
@@ -30,8 +30,6 @@
 #include "libastrochem.h"
 #include "network.h"
 
-#include "input.h"
-
 int add_species (char *new_species, net_t * network);
 
 void realloc_network_species (net_t * network, int n_species);
@@ -155,7 +153,7 @@ read_network (const char *chem_file, net_t * network, const int verbose)
                           &network->reactions[n].reaction_type,
                           &network->reactions[n].reaction_no) != 5)
                 {
-                  input_error (chem_file1, n + 1);
+                  network_file_error (chem_file1, n + 1);
                 }
               break;
             }
@@ -215,7 +213,7 @@ read_network (const char *chem_file, net_t * network, const int verbose)
                     }
                   else
                     {
-                      input_error (chem_file1, n + 1);
+                      network_file_error (chem_file1, n + 1);
                       break;
                     }
                 }
@@ -381,4 +379,21 @@ free_network (net_t * network)
 {
   free (network->reactions);
   free (network->species_names);
+}
+
+/**
+ * @brief Display an error while reading input
+ *
+ * Display an error message and exit of an error is encountered while
+ * reading the input file.
+ *
+ * @param input_file file from wich the error is from
+ * @param line_number line number where the error occured
+ */
+void
+network_file_error (const char *chem_file, int line_number)
+{
+  fprintf (stderr, "astrochem: error: incorrect network file in %s line %i.\n",
+           chem_file, line_number);
+  exit (1);
 }

commit b2bb9efefb48979b631a9540d6e10cf6f50d3e88

Fix OpenMP version
A bug introduced with commit dc646f5b6bee caused OpenMP flags (as well
as other user defined _FLAGS) to be ignored when compiling Astrochem
main program. As a consequence, Astrochem would run serially, even if the
--enable-openmp was passed to the configure script. This is now fixed.

src/Makefile.am
@@ -30,10 +30,10 @@ include_HEADERS = libastrochem.h
 bin_PROGRAMS = astrochem chmconvert converttolegacy
 
 astrochem_SOURCES = astrochem.c 
-astrochem_CFLAGS = ${HDF5_CFLAGS}
-astrochem_CPPFLAGS = ${HDF5_CPPFLAGS}
-astrochem_LDFLAGS = ${HDF5_LDFLAGS}
-astrochem_LDADD = -lastrochem ${HDF5_LIBS}
+astrochem_CFLAGS = ${HDF5_CFLAGS} ${AM_CFLAGS}
+astrochem_CPPFLAGS = ${HDF5_CPPFLAGS} ${AM_CPPFLAGS}
+astrochem_LDFLAGS = ${HDF5_LDFLAGS} ${AM_LDFLAGS}
+astrochem_LDADD = -lastrochem ${HDF5_LIBS} ${AM_LDFLAGS}
 
 chmconvert_SOURCES = chmconvert.py
 chmconvert$(EXEEXT): chmconvert.py

src/astrochem.c
@@ -247,7 +247,7 @@ main (int argc, char *argv[])
           if( full_solve ( fid, dataset, routeDatasets, dataspace, dataspaceRoute, datatype, route_t_datatype, cell_index, &input_params, source_mdl.mode,
                        &source_mdl.cell[cell_index], &network, &source_mdl.ts, verbose) != EXIT_SUCCESS )
             {
-              return EXIT_FAILURE;
+	      exit (EXIT_FAILURE);
             }
           if (verbose >= 1)
             fprintf (stdout, "Done with cell %d.\n", cell_index);