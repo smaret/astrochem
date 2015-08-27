998eab4e97204646935891050bd3a5c9c57955af
Sebastien Maret <sebastien.maret@obs.ujf-grenoble.fr>
2015-08-27 11:51:45 +0200

    Add Coulomb factor for ion/electron-grain reactions
    The reaction rate for electron attachement and ion recombination on
    charged grains now include a Coulombian factor. Since this factor
    depends on the grain charge and the collider (electron/ion), seperate
    reaction types are implemented for each of these:
    
      - Type 24: e(-) + grain
      - Type 25: e(-) + grain(+)
      - Type 26: ion + grain(-)
      - Type 27: ion + grain
    
    The rate are computed following Dimitry Semenov's recommandations.

--- src/rates.c
+++ src/rates.c
@@ -148,8 +148,46 @@ rate (double alpha, double beta, double gamm, int reaction_type,
       }
 
     case 24:
-      /* Gas-grain interaction (excluding depletion and desorption),
-         Electron-grain recombination. */
+      /* Electron attachement on neutral grains */
+
+      /* Note: reactions 24 and 25 differs only by a Coulombian
+	 factor. It would be better to determine the grain charge and
+	 to compute this factor accordingly. Same thing for reactions
+	 26 and 27.*/
+      {
+	double thermal_veloc = pow (8 * CONST_CGSM_BOLTZMANN * tgas
+                                    / (M_PI * CONST_CGSM_MASS_ELECTRON),
+                                    0.5);
+        k = M_PI * pow (grain_size, 2) * alpha * thermal_veloc * 1.3290
+	  * exp (tgas / 20);
+	break;
+      }
+
+    case 25:
+      /* Electron attachment on positively charged grains */
+      {
+	double thermal_veloc = pow (8 * CONST_CGSM_BOLTZMANN * tgas
+                                    / (M_PI * CONST_CGSM_MASS_ELECTRON),
+                                    0.5);
+	double coulomb_factor = 1 + 1.671e-3 / grain_size / tgas;
+        k = M_PI * pow (grain_size, 2) * alpha * thermal_veloc * 1.3290
+	  * exp (tgas / 20) * coulomb_factor;
+	break;
+      }
+
+    case 26:
+      /* Cation recombination on negatively charged grains. */
+      {
+        double thermal_veloc = pow (8 * CONST_CGSM_BOLTZMANN * tgas
+                                    / (M_PI * beta * CONST_CGSM_MASS_PROTON),
+                                    0.5);
+	double coulomb_factor = 1 + 1.671e-3 / grain_size / tgas;
+        k = M_PI * pow (grain_size, 2) * alpha * thermal_veloc * coulomb_factor;
+        break;
+      }
+
+    case 27:
+      /* Cation recombination on neutral grains. */
       {
         double thermal_veloc = pow (8 * CONST_CGSM_BOLTZMANN * tgas
                                     / (M_PI * beta * CONST_CGSM_MASS_PROTON),
