d29c5488b8de63f75e187f650a3c327fd7b40225
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2019-11-22 16:13:53 +0100

    Update SUNDIALS version in the nix shell
    Nixpkgs 19.09 has SUNDIALS 4.1.0, so we don't need to override the
    package anymore.

--- shell.nix
+++ shell.nix
@@ -1,13 +1,6 @@
-with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz") {};
+with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz") {};
 let
   python3Env = pkgs.python3.withPackages (ps: with ps; [ numpy h5py cython matplotlib ]);
-  sundials2 = pkgs.sundials.overrideAttrs (oldAttrs: rec {
-    name = "sundials-2.7.0";
-    src = pkgs.fetchurl {
-      url = "https://computation.llnl.gov/projects/sundials/download/sundials-2.7.0.tar.gz";
-      sha256 = "01513g0j7nr3rh7hqjld6mw0mcx5j9z9y87bwjc16w2x2z3wm7yk";
-    };
-  });
 in
 pkgs.mkShell rec {
   buildInputs = [
@@ -18,7 +11,7 @@ pkgs.mkShell rec {
     pkgs.automake
     pkgs.libtool
     pkgs.ncurses
-    sundials2
     pkgs.openblas
+    pkgs.sundials
   ];
 }
