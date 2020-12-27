ecef75270d90a0069ab068fbd7018741eff13701
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2020-12-27 14:35:24 +0100

    Update nixpkgs version in the nix shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz") {};
+with import
+  (fetchTarball {
+    # Get the revision by choosing a version from https://github.com/nixos/nixpkgs/commits/master
+    url = "https://github.com/nixos/nixpkgs/archive/171144eb2850979d3841949df5a14f048da5d83e.tar.gz";
+    # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
+    sha256 = "0hm78p3avq9khd97x3x1aziribsz0mnrrssanq982yn5kz2jfj6s";
+  })
+{ };
 let
   python3Env = pkgs.python3.withPackages (ps: with ps; [ numpy h5py cython matplotlib ]);
 in
